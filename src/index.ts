82c5f803f80a88649ed050bbab11056bfed6e5f8
Technik97 <aniket.97.mane@gmail.com>
2021-10-23 18:29:44 +0530

    Set up GraphQL, and added a test resolver

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,38 @@
 import "reflect-metadata";
 import express from 'express';
-
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { ApolloServerPluginLandingPageDisabled, ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { PORT } from './secrets'
 
 const main = async () => {
+    const port = PORT || 4000;
+
     const app = express();
 
-    app.listen(4000, () => {
-        console.log(`Server started on localhost:4000`)
+    
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        }),
+        plugins: [
+            ApolloServerPluginLandingPageGraphQLPlayground(),
+            ApolloServerPluginLandingPageDisabled()
+        ]
+    });
+
+    await apolloServer.start();
+
+    apolloServer.applyMiddleware({
+        app, 
+        path: '/api',
+    });
+
+    app.listen(port, () => {
+        console.log(`Server started on localhost:${port}${apolloServer.graphqlPath}`);
     });
 }
 
